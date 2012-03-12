83a7d0bb185a15fa2085e9b09003fe5de574fb47
Dmitry Lihachev <lda@openteam.ru>
2012-03-12 09:40:22 +0700

    rename cropped route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Storage::Application.routes.draw do
     width = [params[:width].to_i, image.file_width].min
     height = [params[:height].to_i, image.file_height].min
     image.file.thumb("#{width}x#{height}#")
-  }, :as => :resized_images, :format => false
+  }, :as => :cropped_images, :format => false
 
   get '/files/:id/:width-:height/*name' => Dragonfly[:files].endpoint { |params, app|
     image = FileEntry.where(:file_mime_directory => 'image').find(params[:id])
