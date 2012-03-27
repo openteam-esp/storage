f423357d6726910387a372ff3ac2c9f4ca114497
Dmitry Lihachev <lda@openteam.ru>
2012-03-27 10:11:10 +0700

    support for zero downtime unicorn reloading

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,32 +1,56 @@
-current_dir = File.expand_path('../..', __FILE__)
-
-project = current_dir.split('/').last
-
-settings = YAML.load_file "#{current_dir}/config/settings.yml"
-
-settings['unicorn'] ||= {}
-settings['unicorn']['workers'] ||= 2
-settings['unicorn']['preload'] = true if settings['unicorn']['preload'] != false
-settings['unicorn']['timeout'] ||= 300
-
-settings['unicorn']['logs_dir'] ||= "/var/log/esp/#{project}"
-settings['unicorn']['pids_dir'] ||= '/var/run/esp'
-settings['unicorn']['socks_dir'] ||= '/tmp'
-unless settings['unicorn']['socks_dir'].start_with? "/"
-  settings['unicorn']['socks_dir'] = "#{current_dir}/#{settings['unicorn']['socks_dir']}"
+def current_dir
+  File.expand_path('../..', __FILE__)
 end
 
-worker_processes  settings['unicorn']['workers']
-preload_app       settings['unicorn']['preload']
-timeout           settings['unicorn']['timeout']
-listen            settings['unicorn']['listen'] if settings['unicorn']['listen']
-listen            "#{settings['unicorn']['socks_dir']}/esp-#{project}.sock", :backlog => 64
-pid               "#{settings['unicorn']['pids_dir']}/#{project}.pid"
-stderr_path       "#{settings['unicorn']['logs_dir']}/stderr.log"
-stdout_path       "#{settings['unicorn']['logs_dir']}/stdout.log"
+def group
+  current_dir.split('/')[-2]
+end
+
+def project
+  current_dir.split('/')[-1]
+end
+
+def config_file_path
+  "#{current_dir}/config/settings.yml"
+end
+
+def settings
+  YAML.load_file(config_file_path)['unicorn'] || {}
+end
+
+def heroku?
+  ENV['PORT']
+end
+
+def pid_file
+  heroku? ? "/tmp/#{group}-#{project}.pid" : "/var/run/#{group}/#{project}.pid"
+end
+
+worker_processes  (settings['workers'] || 2).to_i
+timeout           (settings['timeout'] || 300).to_i
+preload_app       true
+pid               pid_file
+
+if heroku?
+  listen            ENV['PORT'].to_i, :tcp_nopush => false
+else
+  listen            "/tmp/#{group}-#{project}.sock", :backlog => 64
+
+  stdout_path       "/var/log/#{group}/#{project}/stdout.log"
+  stderr_path       "/var/log/#{group}/#{project}/stderr.log"
+end
 
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{pid_file}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
 end
 
 after_fork do |server, worker|
