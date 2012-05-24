215b3c62070eaf4c36f0ded2c8ce6d908e6c0132
Dmitry Lihachev <lda@openteam.ru>
2012-05-24 16:15:30 +0700

    app should raise ActiveRecord::NotFound if have no file within given name

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,21 @@ Storage::Application.routes.draw do
   match 'api/el_finder/v2/*root_path' => 'el_finder/commands#create'
 
   get '/files/:id/:width-:height!/*name' => Dragonfly[:files].endpoint { |params, app|
-    image = FileEntry.where(:file_mime_directory => 'image').find(params[:id])
+    image = FileEntry.where(:file_mime_directory => 'image').where(:name => params[:name]).find(params[:id])
     width = [params[:width].to_i, image.file_width].min
     height = [params[:height].to_i, image.file_height].min
     image.file.thumb("#{width}x#{height}#")
   }, :as => :cropped_images, :format => false
 
   get '/files/:id/:width-:height/*name' => Dragonfly[:files].endpoint { |params, app|
-    image = FileEntry.where(:file_mime_directory => 'image').find(params[:id])
+    image = FileEntry.where(:file_mime_directory => 'image').where(:name => params[:name]).find(params[:id])
     width = [params[:width].to_i, image.file_width].min
     height = [params[:height].to_i, image.file_height].min
     image.file.thumb("#{width}x#{height}")
   }, :as => :resized_images, :format => false
 
   get '/files/:id/*name' => Dragonfly[:files].endpoint { |params, app|
-    app.fetch(FileEntry.where(:id => params[:id]).find_by_name(params[:name]).file_uid)
+    app.fetch(FileEntry.where(:name => params[:name]).find(params[:id]).file_uid)
   }, :as => :files, :format => false
 
   delete '/external_links' => 'external_links#destroy'
