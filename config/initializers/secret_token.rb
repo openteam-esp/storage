4ef348f87aafaebba097fced2bc901a21ec80cd4
Evgeny Lapin <lev@openteam.ru>
2013-08-15 08:42:16 +0700

    update rails, disable default secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Storage::Application.config.secret_token = '26a4fb559da5b9084ed3f8394b5862e8d0d9ff196d3ae72d991b46f54d555837a0c2cd6f4e6a698019840bd1f3a7007ebb54374187387d1be931175a433d6b42'
+# Storage::Application.config.secret_token = '26a4fb559da5b9084ed3f8394b5862e8d0d9ff196d3ae72d991b46f54d555837a0c2cd6f4e6a698019840bd1f3a7007ebb54374187387d1be931175a433d6b42'
