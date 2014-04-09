49be9002a5ecbd38056fc2f51b8abf9a66c983da
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-04-09 16:09:39 +0700

    Little pretty

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,11 @@ module Storage
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W[#{config.root}/app/models/locks #{config.root}/lib/el_finder_api #{config.root}/lib/exceptions]
+    config.autoload_paths += %W(
+                                 #{config.root}/app/models/locks
+                                 #{config.root}/lib/el_finder_api
+                                 #{config.root}/lib/exceptions
+                               )
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
