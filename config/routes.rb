4873014ef2bcae0c2b94c7f9039c39d29595451d
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-04-09 16:11:02 +0700

    Move dragonfly endpoints to separated file

--- config/routes.rb
+++ config/routes.rb
@@ -1,49 +1,11 @@
-DEFAULT_ENDPOINT = Dragonfly[:files].endpoint { |params, app|
-  if params[:width]
-    image = FileEntry.where(:file_mime_directory => 'image').where(:name => params[:name]).find(params[:id])
-    gravity = nil
-
-    if params[:magnify]
-      width = [params[:width].to_i, image.file_width*3, 2000].min
-      height = [params[:height].to_i, image.file_height*3, 2000].min
-    else
-      width = [params[:width].to_i, image.file_width].min
-      height = [params[:height].to_i, image.file_height].min
-    end
-
-    gravity = params[:gravity] if params[:cropify]
-    modificator = '#' if params[:cropify]
-
-    thumbnail = image.file.thumb("#{width}x#{height}#{modificator}#{gravity}").strip
-    thumbnail = thumbnail.process(:watermark) if Settings['watermark']
-
-    thumbnail
-  else
-    base_entry_name, *relative_path = params[:name].split('/')
-    base_entry = FileEntry.where(:name => base_entry_name).find(params[:id])
-    app.fetch base_entry.subfile(relative_path).file_uid
-  end
-}
-
-REGION_ENDPOINT = Dragonfly[:files].endpoint do |params, app|
-  image = FileEntry.where(:file_mime_directory => 'image').where(:name => params[:name]).find(params[:id])
-
-  width, height                 = params[:width], params[:height]
-  x, y                          = params[:x], params[:y]
-  resized_width, resized_height = params[:resized_width], params[:resized_height]
-
-  thumbnail = image.file.thumb("#{width}x#{height}+#{x}+#{y}").strip
-  thumbnail = thumbnail.process(:watermark) if Settings['watermark']
-  thumbnail = thumbnail.thumb("#{resized_width}x#{resized_height}") if resized_width && resized_height
-
-  thumbnail
-end
+require Rails.root.join('lib', 'endpoints')
 
 Storage::Application.routes.draw do
   match 'api/el_finder/v2' => 'el_finder/commands#create'
   match 'api/el_finder/v2/*root_path' => 'el_finder/commands#create'
 
-  get '/files/:id/region/:width/:height/:x/:y/(:resized_width-:resized_height)/*name' => REGION_ENDPOINT,
+  # get image's region with watermark
+  get '/files/:id/region/:width/:height/:x/:y/(:resized_width-:resized_height)/*name' => Endpoints.region { |thumbnail| Settings['watermark'] ? thumbnail.process(:watermark) : thumbnail },
     :defaults => { :watermark => true },
     :constraints => { :width => /\d+/,
                       :height => /\d+/,
@@ -53,13 +15,34 @@ Storage::Application.routes.draw do
                       :resized_height => /\d+/ },
     :format => false
 
-  get '/files/:id/(:width-:height(:cropify)(:magnify)(:gravity))/*name' => DEFAULT_ENDPOINT,
+  # get image's region without watermark (o = original)
+  get '/o/files/:id/region/:width/:height/:x/:y/(:resized_width-:resized_height)/*name' => Endpoints.region,
+    :defaults => { :watermark => true },
+    :constraints => { :width => /\d+/,
+                      :height => /\d+/,
+                      :x => /\d+/,
+                      :y => /\d+/,
+                      :resized_width => /\d+/,
+                      :resized_height => /\d+/ },
+    :format => false
+
+  # get images with watermark by default
+  get '/files/:id/(:width-:height(:cropify)(:magnify)(:gravity))/*name' => Endpoints.default { |thumbnail| Settings['watermark'] ? thumbnail.process(:watermark) : thumbnail },
     :constraints => { :width => /\d+/,
                       :height => /\d+/,
                       :cropify => /(\!|c)/,
                       :magnify => /m/,
                       :gravity => /(e|ne|n|nw|se|s|sw|w)/ },
     :as => :files,
+    :format => false
+
+  # get images without watermark (o = original)
+  get '/o/files/:id/(:width-:height(:cropify)(:magnify)(:gravity))/*name' => Endpoints.default,
+    :constraints => { :width => /\d+/,
+                      :height => /\d+/,
+                      :cropify => /(\!|c)/,
+                      :magnify => /m/,
+                      :gravity => /(e|ne|n|nw|se|s|sw|w)/ },
     :format => false
 
   delete '/external_links' => 'external_links#destroy'
---
+++ lib/endpoints.rb
@@ -0,0 +1,51 @@
+class Endpoints
+  def self.default(&block)
+    Dragonfly[:files].endpoint do |params, app|
+      if params[:width]
+        image = FileEntry.where(:file_mime_directory => 'image').where(:name => params[:name]).find(params[:id])
+        gravity = nil
+
+        if params[:magnify]
+          width = [params[:width].to_i, image.file_width*3, 2000].min
+          height = [params[:height].to_i, image.file_height*3, 2000].min
+        else
+          width = [params[:width].to_i, image.file_width].min
+          height = [params[:height].to_i, image.file_height].min
+        end
+
+        gravity = params[:gravity] if params[:cropify]
+        modificator = '#' if params[:cropify]
+
+        thumbnail = image.file.thumb("#{width}x#{height}#{modificator}#{gravity}").strip
+
+        thumbnail = block.call(thumbnail) if block
+
+        thumbnail
+      else
+        base_entry_name, *relative_path = params[:name].split('/')
+        base_entry = FileEntry.where(:name => base_entry_name).find(params[:id])
+        app.fetch base_entry.subfile(relative_path).file_uid
+      end
+    end
+  end
+
+  def self.region(&block)
+    Dragonfly[:files].endpoint do |params, app|
+      p params
+      image = FileEntry.where(:file_mime_directory => 'image').where(:name => params[:name]).find(params[:id])
+
+      width, height                 = params[:width], params[:height]
+      x, y                          = params[:x], params[:y]
+      resized_width, resized_height = params[:resized_width], params[:resized_height]
+
+      thumbnail = image.file.thumb("#{width}x#{height}+#{x}+#{y}").strip
+
+      thumbnail = block.call(thumbnail) if block
+
+      thumbnail = thumbnail.thumb("#{resized_width}x#{resized_height}") if resized_width && resized_height
+
+      thumbnail
+    end
+  end
+end
+
